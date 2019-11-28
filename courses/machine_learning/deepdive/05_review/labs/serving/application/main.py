c4b0cdf2e71395554874d63057a6929be35c5d16
Benoit Dherin <dherin@google.com>
2019-11-28 17:47:19 +0900

    removed key field from the ml payload causing the deployed ml api to crash

--- courses/machine_learning/deepdive/05_review/labs/serving/application/main.py
+++ courses/machine_learning/deepdive/05_review/labs/serving/application/main.py
@@ -76,7 +76,6 @@ def predict():
       return jsonify({'result': 'Set all items.'})
 
   features = {}
-  features['key'] = 'nokey'
   features['is_male'] = gender2str(data['baby_gender'])
   features['mother_age'] = float(data['mother_age'])
   features['plurality'] = plurality2str(data['plurality'])
